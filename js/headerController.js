7369c18cf8c93a24183dbfd6fb26832ba9442646
B31l <rnfhrn960@gmail.com>
2021-12-11 00:32:25 +0900

    alter header

--- js/headerController.js
+++ js/headerController.js
@@ -1,5 +1,5 @@
 function 범례(이름) {
-    legends = ["legendArchives", "legendCategories", "legendTags"];
+    legends = ["legendArchives", "legendCategories", "legendTags", "legendSearch", "legendMore"];
     legends.forEach(element => {
         if (element === 이름) {
             document.getElementById(element).style.display = "block";

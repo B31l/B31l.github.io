6d26dafe953ac602d6e3d62108ebbb368f024a24
B31l <rnfhrn960@gmail.com>
2021-12-11 14:39:33 +0900

    ㅊ

--- js/main.js
+++ js/main.js
@@ -7,26 +7,26 @@
 /**
  * clickMenu
  */
-(function() {
-  if (window.innerWidth <= 770) {
-    var menuBtn = document.querySelector('#headerMenu')
-    var nav = document.querySelector('#headerNav')
-    menuBtn.onclick = function(e) {
-      e.stopPropagation()
-      if (menuBtn.classList.contains('active')) {
-        menuBtn.classList.remove('active')
-        nav.classList.remove('nav-show')
-      } else {
-        nav.classList.add('nav-show')
-        menuBtn.classList.add('active')
-      }
-    }
-    document.querySelector('body').addEventListener('click', function() {
-      nav.classList.remove('nav-show')
-      menuBtn.classList.remove('active')
-    })
-  }
-}());
+// (function() {
+//   if (window.innerWidth <= 770) {
+//     var menuBtn = document.querySelector('#headerMenu')
+//     var nav = document.querySelector('#headerNav')
+//     menuBtn.onclick = function(e) {
+//       e.stopPropagation()
+//       if (menuBtn.classList.contains('active')) {
+//         menuBtn.classList.remove('active')
+//         nav.classList.remove('nav-show')
+//       } else {
+//         nav.classList.add('nav-show')
+//         menuBtn.classList.add('active')
+//       }
+//     }
+//     document.querySelector('body').addEventListener('click', function() {
+//       nav.classList.remove('nav-show')
+//       menuBtn.classList.remove('active')
+//     })
+//   }
+// }());
 
 //////////////////////////back to top////////////////////////////
 (function() {

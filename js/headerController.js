d6414b637cb640f736ef49b02d720d9bda9462db
B31l <rnfhrn960@gmail.com>
2021-12-11 14:54:55 +0900

    try

--- js/headerController.js
+++ js/headerController.js
@@ -1,10 +1,9 @@
-function 범례(이름) {
-    legends = ["legendArchives", "legendCategories", "legendTags", "legendSearch", "legendMore"];
-    legends.forEach(element => {
-        if (element === 이름) {
-            document.getElementById(element).style.display = "block";
-        } else {
-            document.getElementById(element).style.display = "none";
-        }
+function 범례(idx) {
+    legends = ["Search", "Archives", "Categories", "Tags", "More"];
+    legends.forEach(Element => {
+        document.getElementById(`legend${Element}`).style.display = "none";
+        document.getElementById(`btn${Element}`).style.color = "WHITE";
     });
+    document.getElementById(`legend${legends[idx]}`).style.display = "block";
+    document.getElementById(`btn${legends[idx]}`).style.color = "#FFCC33";
 }

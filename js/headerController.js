fdf4ec9fcdfa2ef6866967b0758f8c9cb402a40a
B31l <rnfhrn960@gmail.com>
2021-12-14 00:31:42 +0900

    이것저것

--- js/headerController.js
+++ js/headerController.js
@@ -1,5 +1,5 @@
 function 범례(idx) {
-    legends = ["Search", "Archives", "Categories", "Tags", "More"];
+    legends = ["Search", "Archives", "Categories", "Tags", "Demo", "Setting", "More"];
     legends.forEach(Element => {
         document.getElementById(`legend${Element}`).style.display = "none";
         document.getElementById(`btn${Element}`).style.color = "WHITE";
@@ -7,16 +7,4 @@ function 범례(idx) {
     localStorage.setItem('LEGEND_KEY', idx);
     document.getElementById(`legend${legends[idx]}`).style.display = "block";
     document.getElementById(`btn${legends[idx]}`).style.color = "#FFCC33";
-}
-
-let activation;
-function 설정() {
-    const settingDisplay = document.getElementById('settingBox')
-    if (activation === false) {
-        settingDisplay.style.display = "block";
-        activation = true;
-    } else {
-        settingDisplay.style.display = "none"
-        activation = false;
-    }
 }

8800d946928b6f0cd91f247f34f8115653b2befe
B31l <rnfhrn960@gmail.com>
2021-12-29 00:14:49 +0900

    Update headerController.js

--- js/headerController.js
+++ js/headerController.js
@@ -1,9 +1,16 @@
+let presentBtn = null;
+
 function 범례(idx) {
     legends = ["Setting", "Search", "Archives", "Categories", "Tags", "Demo", "More"];
     legends.forEach(Element => {
         document.getElementById(`legend${Element}`).style.display = "none";
         document.getElementById(`btn${Element}`).style.color = "WHITE";
     });
+    if (presentBtn === idx) {
+        presentBtn = null;
+        return
+    }
+    presentBtn = idx
     localStorage.setItem('LEGEND_KEY', idx);
     document.getElementById(`legend${legends[idx]}`).style.display = "block";
     document.getElementById(`btn${legends[idx]}`).style.color = "#48D33A";

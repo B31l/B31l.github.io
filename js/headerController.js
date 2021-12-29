75dfb0b51ef532f5036a6da9517b3a9f588b18de
B31l <rnfhrn960@gmail.com>
2021-12-29 16:52:10 +0900

    Update headerController.js

--- js/headerController.js
+++ js/headerController.js
@@ -3,30 +3,31 @@ let presentBtn = null;
 const container = document.querySelector('.container');
 const searchInput = document.getElementById("search-input");
 const legendSearch = document.getElementById("legendSearch");
+const legends = ["Setting", "Search", "Archives", "Categories", "Tags"];
 
-function 범례(idx) {
-    legends = ["Setting", "Search", "Archives", "Categories", "Tags"];
+function initialize() {
     legends.forEach(Element => {
+        container.style.display = "none"
         document.getElementById(`legend${Element}`).style.display = "none";
         document.getElementById(`btn${Element}`).style.color = "#9d9d9d";
     });
-    if (presentBtn === idx) {
+}
+function 범례(idx) {    
+    initialize();
+    if (presentBtn === idx) {   // 클릭 취소
         presentBtn = null;
-        // localStorage.setItem('LEGEND_KEY', null);
-        container.style.display = "none"
-    }
-    else {
+    } else {                    // 다른 버튼 클릭
         presentBtn = idx
-        // localStorage.setItem('LEGEND_KEY', idx);
-        container.style.display = "block"
+        container.style.display = "block";
         document.getElementById(`legend${legends[idx]}`).style.display = "block";
         document.getElementById(`btn${legends[idx]}`).style.color = "#48D33A";
     }
 }
 
 if (searchInput.value !== "") {
-    container.style.display = "block"
-    legendSearch.style.display = "block"
+    initialize();
+    container.style.display = "block";
+    legendSearch.style.display = "block";
 } else {
     container.style.display = "none"
     legendSearch.style.display = "none"

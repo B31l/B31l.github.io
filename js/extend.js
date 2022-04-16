13acd70623223d3a275733eb5f709da2bdac72d1
B31l <rnfhrn960@gmail.com>
2022-04-17 01:04:27 +0900

    c

--- js/extend.js
+++ js/extend.js
@@ -1,14 +1,9 @@
-const page = document.querySelector(".page");
 const section = document.querySelector("section");
 let state = false;
 function extend() {
-    if (!state) {
-        page.style.width = "100%";
+    if (!state)
         section.style.width = "calc(100% - 340px)";
-    }
-    else {
-        page.style.width = "1200px";
+    else
         section.style.width = "770px";
-    }
     state = !state;
 }

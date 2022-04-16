39eacec0263fd4c25b2ea0dcb63457fc074a7a17
B31l <rnfhrn960@gmail.com>
2022-04-17 01:29:44 +0900

    Update extend.js

--- js/extend.js
+++ js/extend.js
@@ -1,16 +1,16 @@
 const page = document.querySelector(".page");
 const section = document.querySelector("section");
 
-let state = false;
+let state = "off"
 function extend() {
     state = window.localStorage.getItem("state");
-    if (!state) {
-        window.localStorage.setItem("state", true);
+    if (state === "off") {
+        window.localStorage.setItem("state", "on");
         page.style.width = "100%";
         section.style.width = "calc(100% - 340px)";
     }
     else {
-        window.localStorage.setItem("state", false);
+        window.localStorage.setItem("state", "off");
         page.style.width = "1200px";
         section.style.width = "770px";
     }

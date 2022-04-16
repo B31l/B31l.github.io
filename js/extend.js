c296fa794c83a80ca6f5a241132c1c4e97ac1fd7
B31l <rnfhrn960@gmail.com>
2022-04-17 01:22:22 +0900

    y

--- js/extend.js
+++ js/extend.js
@@ -1,14 +1,18 @@
 const page = document.querySelector(".page");
 const section = document.querySelector("section");
+
 let state = false;
+state = window.localStorage.getItem("state");
+window.localStorage.setItem('age', '20');
 function extend() {
     if (!state) {
         page.style.width = "100%";
         section.style.width = "calc(100% - 340px)";
+        window.localStorage.setItem("state", true);
     }
     else {
         page.style.width = "1200px";
         section.style.width = "770px";
+        window.localStorage.setItem("state", false);
     }
-    state = !state;
 }

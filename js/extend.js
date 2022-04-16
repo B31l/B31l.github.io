1e581d4f6cfa057d367896b5652f034dabe2f4be
B31l <rnfhrn960@gmail.com>
2022-04-17 01:32:09 +0900

    Update extend.js

--- js/extend.js
+++ js/extend.js
@@ -1,17 +1,17 @@
 const page = document.querySelector(".page");
 const section = document.querySelector("section");
 
-let state = "off"
+let state = "off";
 function extend() {
     state = window.localStorage.getItem("state");
-    if (state === "off") {
-        window.localStorage.setItem("state", "on");
-        page.style.width = "100%";
-        section.style.width = "calc(100% - 340px)";
-    }
-    else {
-        window.localStorage.setItem("state", "off");
+    if (state === "on") {
         page.style.width = "1200px";
         section.style.width = "770px";
+        window.localStorage.setItem("state", "off");
+    }
+    else {
+        page.style.width = "100%";
+        section.style.width = "calc(100% - 340px)";
+        window.localStorage.setItem("state", "on");
     }
 }

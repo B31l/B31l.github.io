5615c80d6c210d2d14cace6392f2b381a95fa244
B31l <rnfhrn960@gmail.com>
2022-04-17 00:59:29 +0900

    Update extend.js

--- js/extend.js
+++ js/extend.js
@@ -1,15 +1,14 @@
-const state = false;
 const page = document.querySelector(".page");
 const section = document.querySelector("section");
+let state = false;
 function extend() {
     if (!state) {
         page.style.width = "100%";
         section.style.width = "calc(100% - 340px)";
-        state = true;
     }
     else {
         page.style.width = "1200px";
         section.style.width = "770px";
-        state = false;
     }
+    state = !state;
 }

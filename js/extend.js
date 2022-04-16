61355eea5df36ca648d959a7f41f5199c2159f19
B31l <rnfhrn960@gmail.com>
2022-04-17 00:40:37 +0900

    c

---
+++ js/extend.js
@@ -0,0 +1,14 @@
+const state = false;
+const page = document.querySelector(".page")
+const section = document.querySelector("section");
+function extend() {
+    if (!state) {
+        page.style.width = "100%";
+        section.style.width = "calc(100% - 340px)";
+    }
+    else {
+        page.style.width = "1200px";
+        section.style.width = "770px";
+    }
+    !state;
+}

c1dc62a63483eb4cb4531b0d3fe72be081485228
Sujong Ham <rnfhrn960@gmail.com>
2022-12-24 21:46:52 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,14 @@
+const banner = document.querySelector(".post_banner");
+const toc = document.querySelector("#markdown-toc");
+
 const a = () => console.log("ㅎㅇ");
+
+const turn = () => {
+  if (banner.style.display === "none") {
+    banner.style.display = "block";
+    toc.style.display = "block";
+  } else {
+    banner.style.display = "none";
+    toc.style.display = "none";
+  }
+};

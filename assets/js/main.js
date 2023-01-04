af895bbf0eeb53c74c2ca9c4db8c79af287548a3
Sujong Ham <rnfhrn960@gmail.com>
2023-01-04 20:26:46 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,18 @@
+var scroller = {};
+scroller.e = document.querySelector(".main");
+if (scroller.e.addEventListener) {
+  scroller.e.addEventListener("mousewheel", MouseWheelHandler, false);
+  scroller.e.addEventListener("DOMMouseScroll", MouseWheelHandler, false);
+} else scroller.e.attachEvent("onmousewheel", MouseWheelHandler);
+function MouseWheelHandler(e) {
+  var e = window.event || e;
+  var delta = -80 * Math.max(-1, Math.min(1, e.wheelDelta || -e.detail));
+  var pst = document.querySelector(".main").scrollLeft + delta;
+  if (pst < 0) pst = 0;
+  document.querySelector(".main").scrollLeft = pst;
+  return false;
+}
+
 const toggleTOC = () => {
   const toc = document.querySelector("#markdown-toc");
   toc.style.display = toc.style.display === "block" ? "none" : "block";
@@ -6,7 +21,6 @@ const toggleTOC = () => {
 
 function init() {
   const main = document.querySelector(".main");
-
   const list_hr = document.querySelectorAll("hr");
   const mainCurrent = main.getBoundingClientRect().bottom;
   list_hr.forEach((item) => {
@@ -29,77 +43,25 @@ function init() {
   });
 }
 
-// document.addEventListener("scroll", horizontalScroll);
-
-// const main = document.querySelector(".main");
-// const content = document.querySelector(".content");
-
-// let scrollWidth = main.scrollWidth;
-// let verticalScrollHeight =
-//   content.getBoundingClientRect().height - main.getBoundingClientRect().height;
-
-// function horizontalScroll() {
-//   let position = main.getBoundingClientRect().top;
-//   if (position > 1) {
-//     return;
-//   } else {
-//     let scrolled = content.getBoundingClientRect().top;
-//     main.scrollLeft = (scrollWidth / verticalScrollHeight) * scrolled * -0.85;
-//   }
-// }
+function setTOC() {
+  const toc = document.querySelector("#markdown-toc");
+  toc.children.forEach((item_h1, index_h1) => {
+    if (item_h1.childElementCount === 1) {
+      item_h1.innerHTML = `<div class="toc_h1">${index_h1 + 1}</div>${
+        item.innerHTML
+      }`;
+    } else {
+      item_h1.children.forEach((item_h2, index_h2) => {
+        if (item_h2.childElementCount === 1) {
+          item_h2.innerHTML = `<div class="toc_h2">${index_h1 + 1} - ${
+            index_h2 + 1
+          }</div>${item.innerHTML}`;
+        } else {
+          console.log("히히");
+        }
+      });
+    }
+  });
+}
 
 init();
-
-// const main = document.querySelector(".main");
-// let isMouseDown = false;
-// let startX, scrollLeft;
-
-// main.addEventListener("mousedown", (e) => {
-//   isMouseDown = true;
-//   main.classList.add("active");
-
-//   startX = e.pageX - main.offsetLeft;
-//   scrollLeft = main.scrollLeft;
-// });
-
-// main.addEventListener("mouseleave", () => {
-//   isMouseDown = false;
-//   main.classList.remove("active");
-// });
-
-// main.addEventListener("mouseup", () => {
-//   isMouseDown = false;
-//   main.classList.remove("active");
-// });
-
-// main.addEventListener("mousemove", (e) => {
-//   if (!isMouseDown) return;
-
-//   e.preventDefault();
-//   const x = e.pageX - main.offsetLeft;
-//   const walk = (x - startX) * 1;
-//   main.scrollLeft = scrollLeft - walk;
-// });
-
-var scroller = {};
-scroller.e = document.querySelector(".main");
-
-if (scroller.e.addEventListener) {
-  scroller.e.addEventListener("mousewheel", MouseWheelHandler, false);
-  scroller.e.addEventListener("DOMMouseScroll", MouseWheelHandler, false);
-} else scroller.e.attachEvent("onmousewheel", MouseWheelHandler);
-
-function MouseWheelHandler(e) {
-  var e = window.event || e;
-  var delta = -80 * Math.max(-1, Math.min(1, e.wheelDelta || -e.detail));
-
-  var pst = document.querySelector(".main").scrollLeft + delta;
-
-  if (pst < 0) {
-    pst = 0;
-  }
-
-  document.querySelector(".main").scrollLeft = pst;
-
-  return false;
-}

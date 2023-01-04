6cbbefb922bf6c613ccf529f26ed4510bd8d0a29
Sujong Ham <rnfhrn960@gmail.com>
2023-01-04 10:26:54 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,7 +24,7 @@ function init() {
 
   const list_img = document.querySelectorAll("img");
   list_img.forEach((item) => {
-    item.innerHTML = `<div class="img-box">${item.alt}</div>${item.innerHTML}`;
+    item.parentNode.innerHTML = `<div class="img-box">${item.alt}</div>${item}`;
   });
 }
 

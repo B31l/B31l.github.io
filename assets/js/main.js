a6e38b0db2227a9460bfbeb60ea8815663af4688
Sujong Ham <rnfhrn960@gmail.com>
2023-01-04 10:19:37 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,17 +8,23 @@ const toggleTOC = () => {
 };
 
 function init() {
+  const main = document.querySelector(".main");
+
+  const list_hr = document.querySelectorAll("hr");
+  const mainCurrent = main.getBoundingClientRect().bottom;
+  list_hr.forEach((item) => {
+    const hrCurrent = item.getBoundingClientRect().bottom;
+    item.style.marginBottom = (mainCurrent - hrCurrent).toString() + "px";
+  });
+
   const list_h1 = document.querySelectorAll("h1");
   list_h1.forEach((item, index) => {
     item.innerHTML = `<div class="h1-box">${index + 1}</div>${item.innerHTML}`;
   });
 
-  const list_hr = document.querySelectorAll("hr");
-  const main = document.querySelector(".main");
-  const mainCurrent = main.getBoundingClientRect().bottom;
-  list_hr.forEach((item) => {
-    const hrCurrent = item.getBoundingClientRect().bottom;
-    item.style.marginBottom = (mainCurrent - hrCurrent).toString() + "px";
+  const list_img = document.querySelectorAll("img");
+  list_img.forEach((item) => {
+    item.innerHTML = `<div class="img-box">${item.alt}</div>${item.innerHTML}`;
   });
 }
 
